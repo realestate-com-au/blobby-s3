e96959d14f8a097ddb095bf9abf0f3fef82ed4c4
Mike Williams <mdub@dogbiscuit.org>
2015-06-15 23:10:55 +1000

    Remove support for mirror buckets, and logging.
    Mirroring is now better achieved using S3 cross-region replication.

--- lib/blobby/s3_store.rb
+++ lib/blobby/s3_store.rb
@@ -13,30 +13,22 @@ module Blobby
 
     # Create a new instance.
     #
-    # If more than one bucket_name is specified, the store will write synchronously
-    # to the first bucket, and asynchronously to the rest.
-    #
-    # bucket_names - name of bucket(s) to store things in
+    # bucket_name  - name of the bucket to store things in
     # object_acl   - a canned access control policy
     # s3_options   - options passed to AWS::S3.new
     #
-    def initialize(bucket_names, object_acl = :private, logger = nil, s3_options = {})
-      @bucket_names = Array(bucket_names).map(&:to_str)
+    def initialize(bucket_name, object_acl = :private, s3_options = {})
+      @bucket_name = bucket_name.to_str
       @s3_options = s3_options.dup
       @s3_options.freeze
-      @logger = logger || Logger.new("/dev/null")
       @object_acl = object_acl || :private  # Reset to AWS default if explicitly set to nil
     end
 
-    attr_reader :bucket_names
+    attr_reader :bucket_name
     attr_reader :s3_options
 
-    def bucket_name
-      bucket_names.first
-    end
-
     def available?
-      buckets.first.objects.first
+      bucket.objects.first
       true
     rescue ::AWS::Errors::Base
       false
@@ -44,110 +36,44 @@ module Blobby
 
     def [](key)
       KeyConstraint.must_allow!(key)
-      copies = buckets.map { |bucket| bucket.objects[key] }
-      StoredObject.new(copies, object_acl, logger)
+      StoredObject.new(bucket.objects[key], object_acl)
     end
 
     class StoredObject
 
-      def initialize(copies, acl, logger = nil)
-        @copies = copies
-        @logger = logger
+      def initialize(s3_object, acl)
+        @s3_object = s3_object
         @acl = acl
       end
 
       def exists?
-        primary_copy.exists?
+        s3_object.exists?
       end
 
       def read(&block)
-        return nil unless primary_copy.exists?
+        return nil unless s3_object.exists?
         if block_given?
-          primary_copy.read(&block)
+          s3_object.read(&block)
           nil
         else
-          primary_copy.read
+          s3_object.read
         end
       end
 
       def write(payload)
-        write_primary(payload)
-        mirror_in_background
+        s3_object.write(payload, :acl => acl)
         nil
       end
 
       def delete
-        primary_result = delete_primary
-        mirror_deletes_in_background
-        primary_result
+        return false unless s3_object.exists?
+        s3_object.delete
+        true
       end
 
       private
 
-      def write_primary(payload)
-        with_logging("wrote", "failed to write", object_url(primary_copy)) do
-          primary_copy.write(payload, :acl => acl)
-        end
-      end
-
-      def delete_primary
-        delete_object(primary_copy)
-      end
-
-      def with_logging(did, did_not, thing)
-        start = Time.now
-        yield
-        logger.info { "#{did} #{thing} in #{Time.now - start}s" }
-      rescue => e
-        logger.error { "#{did_not} #{thing}" }
-        logger.error { exception_message(e) }
-        raise e
-      end
-
-      def exception_message(e)
-        lines = [e.message]
-        e.backtrace.each do |backtrace_line|
-          lines << ("  " + backtrace_line)
-        end
-        lines.join("\n")
-      end
-
-      def mirror_in_background
-        secondary_copies.each do |copy|
-          Thread.new do
-            with_logging("copied", "failed to copy", "#{object_url(primary_copy)} to #{object_url(copy)}") do
-              copy.copy_from(primary_copy, :acl => acl)
-            end
-          end
-        end
-      end
-
-      def mirror_deletes_in_background
-        secondary_copies.each do |copy|
-          Thread.new { delete_object(copy) }
-        end
-      end
-
-      def delete_object(object)
-        return false unless object.exists?
-        with_logging("deleted", "failed to delete", object_url(object)) do
-          object.delete
-        end
-        true
-      end
-
-      def object_url(o)
-        "s3://#{o.bucket.name}/#{o.key}"
-      end
-
-      def primary_copy
-        @copies.first
-      end
-
-      def secondary_copies
-        @copies.drop(1)
-      end
-
+      attr_reader :s3_object
       attr_reader :logger
       attr_reader :acl
 
@@ -162,10 +88,8 @@ module Blobby
       ::AWS::S3.new(defaults.merge(s3_options))
     end
 
-    def buckets
-      bucket_names.map do |name|
-        s3_client.buckets[name]
-      end
+    def bucket
+      s3_client.buckets[bucket_name]
     end
 
     attr_reader :logger
--- spec/blobby/s3_store_spec.rb
+++ spec/blobby/s3_store_spec.rb
@@ -43,6 +43,7 @@ describe Blobby::S3Store do
   end
 
   describe "#delete" do
+
     let(:key) { "my_key" }
 
     before do
@@ -53,6 +54,7 @@ describe Blobby::S3Store do
     it "removes stuff from S3" do
       expect(bucket.objects[key]).to_not exist
     end
+
   end
 
   context "when we can't talk to S3" do
@@ -76,186 +78,6 @@ describe Blobby::S3Store do
     end
 
     it { is_expected.not_to be_available }
-
-  end
-
-  let(:log_buffer) { StringIO.new }
-  let(:logger) { Logger.new(log_buffer) }
-  let(:log_output) { log_buffer.string }
-
-  context "with a Logger" do
-    let(:key) { "foo" }
-
-    subject do
-      described_class.new(bucket_name, {}, logger)
-    end
-
-    context "when successful" do
-      before do
-        subject[key].write("bar")
-      end
-
-      describe "#write" do
-        it "logs the write" do
-          expect(log_output).to include(%(wrote s3://test-bucket/foo))
-        end
-      end
-
-      describe "#delete" do
-        it "logs the delete" do
-          subject[key].delete
-          expect(log_output).to include(%(deleted s3://test-bucket/foo))
-        end
-      end
-    end
-
-    context "when unsuccessful" do
-      describe "#write" do
-        let(:do_write) { subject[key].write("bar") }
-
-        before do
-          allow(bucket.objects[key]).to receive(:write) { fail "something bad happened" }
-        end
-
-        it "logs the error" do
-          do_write rescue nil
-          expect(log_output).to include(%(failed to write s3://test-bucket/foo))
-        end
-
-        it "re-throws the exception" do
-          expect { do_write }.to raise_error("something bad happened")
-        end
-      end
-
-      describe "#delete" do
-        let(:do_delete) { subject[key].delete }
-
-        before do
-          subject[key].write("bar")
-          allow(bucket.objects[key]).to receive(:delete) { fail "something bad happened" }
-        end
-
-        it "logs the error" do
-          do_delete rescue nil
-          expect(log_output).to include(%(failed to delete s3://test-bucket/foo))
-        end
-
-        it "re-throws the exception" do
-          expect { do_delete }.to raise_error("something bad happened")
-        end
-      end
-    end
-  end
-
-  context "with multiple buckets" do
-
-    let(:bucket_names) { %w(bucket1 bucket2 bucket3) }
-
-    subject do
-      described_class.new(bucket_names, {}, logger)
-    end
-
-    let(:primary_bucket) { fake_s3.buckets["bucket1"] }
-    let(:secondary_buckets) { [fake_s3.buckets["bucket2"], fake_s3.buckets["bucket3"]] }
-
-    before do
-      @background_jobs = []
-      allow(Thread).to receive(:new) do |&block|
-        @background_jobs << block
-      end
-    end
-
-    def run_background_jobs
-      @background_jobs.each do |block|
-        block.call rescue nil
-      end
-    end
-
-    context "when writing or deleting objects" do
-
-      let(:key) { "data/file" }
-      let(:content) { "CONTENT" }
-
-      before do
-        subject[key].write(content)
-      end
-
-      describe "#write" do
-        it "writes to the first bucket" do
-          expect(primary_bucket.objects[key].read).to eq(content)
-        end
-
-        it "doesn't write to the other buckets" do
-          secondary_buckets.each do |bucket_name|
-            expect(bucket_name.objects[key]).to_not exist
-          end
-        end
-      end
-
-      describe "#delete" do
-        before do
-          run_background_jobs # write to other buckets
-          subject[key].delete
-        end
-
-        it "deletes from the first bucket" do
-          expect(primary_bucket.objects[key]).to_not exist
-        end
-
-        it "doesn't delete from the other buckets" do
-          secondary_buckets.each do |bucket_name|
-            expect(bucket_name.objects[key]).to exist
-          end
-        end
-      end
-
-      context "asynchronously" do
-        let(:inaccessible_bucket) { secondary_buckets.first }
-        let(:accessible_bucket) { secondary_buckets.last }
-
-        describe "#write" do
-          before do
-            allow(inaccessible_bucket.objects[key]).to receive(:write) { fail "something bad happened" }
-            run_background_jobs
-          end
-
-          it "tries to write to the remaining buckets" do
-            expect(accessible_bucket.objects[key].read).to eq(content)
-          end
-
-          it "logs successful copies" do
-            expect(log_output).to include(%(copied s3://bucket1/#{key} to s3://#{accessible_bucket.name}/#{key}))
-          end
-
-          it "logs failed copies" do
-            expect(log_output).to include(%(failed to copy s3://bucket1/#{key} to s3://#{inaccessible_bucket.name}/#{key}))
-          end
-        end
-
-        describe "#delete" do
-          before do
-            run_background_jobs # write to the other buckets
-            subject[key].delete
-
-            allow(inaccessible_bucket.objects[key]).to receive(:delete) { fail "something bad happened" }
-            run_background_jobs
-          end
-
-          it "deletes from the remaining buckets" do
-            expect(accessible_bucket.objects[key]).to_not exist
-          end
-
-          it "logs successful delete" do
-            expect(log_output).to include(%(deleted s3://#{accessible_bucket.name}/#{key}))
-          end
-
-          it "logs failed delete" do
-            expect(log_output).to include(%(failed to delete s3://#{inaccessible_bucket.name}/#{key}))
-          end
-        end
-      end
-
-    end
 
   end
 
